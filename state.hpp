c1b5872b921a1b2161d0328fc0e658c06b6a4759
Anbcodes <me@anb.codes>
2025-01-10 15:49:34 -0500

    something

--- state.hpp
+++ state.hpp
@@ -4,19 +4,22 @@
 
 #include "consts.hpp"
 
-enum class Mode {
+enum class Mode
+{
     Clock = 0,
     Stopwatch = 1,
 };
 
-enum class Op {
+enum class Op
+{
     None = -1,
     End = 0,
     Get = 1,
     Set = 2,
 };
 
-enum class Variable {
+enum class Variable
+{
     HourColor = 1,
     MinuteColor,
     SecondColor,
@@ -25,16 +28,20 @@ enum class Variable {
     StartTime,
     PrevTime,
     ShowMs,
+    Brightness,
+    On,
+    TimeZoneOffset,
 };
 
-struct State {
+struct State
+{
     CRGB hourColor = CRGB(255, 0, 0);
     CRGB minuteColor = CRGB(0, 255, 0);
     CRGB secondColor = CRGB(0, 0, 255);
     CRGB msColor = CRGB(0, 255, 255);
 
     uint8_t brightness = 200;
-    int timeZoneOffset = -4;  // EDT
+    int8_t timeZoneOffset = -4; // EDT
 
     Mode mode = Mode::Clock;
 
@@ -42,4 +49,6 @@ struct State {
     unsigned long long stopwatchPrevTime = 0;
 
     bool showMs = false;
+
+    bool on = true;
 };
